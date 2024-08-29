c641bc24d87977ebb99d9e957d19bebbea85e7dd
Jhonny Cardona <Jhonncard@gmail.com>
2024-08-29 02:51:06 -0400

    cambio para la ejecion de pruebas

--- src/domain/use-cases/create-table.use-case.ts
+++ src/domain/use-cases/create-table.use-case.ts
@@ -1,5 +1,5 @@
 export interface CreateTableUseCase {
-  execute: (options: CreateTableOptions) => string;
+  execute: ( options: CreateTableOptions ) => string;
 }
 
 export interface CreateTableOptions {
@@ -7,17 +7,23 @@ export interface CreateTableOptions {
   limit?: number;
 }
 
-export class CreateTable implements CreateTableUseCase {
-  constructor() /**
-   * DI Dependency Injection
-   */
-  {}
 
-  execute({ base, limit = 10 }: CreateTableOptions) {
-    let outputMessage = "";
-    for (let i = 1; i <= limit; i++) {
-      outputMessage += `${base} x ${i} = ${base * i}\n`;
+export class CreateTable implements CreateTableUseCase {
+
+  constructor(
+    /**
+     * DI - Dependency Injection
+     */
+  ){}
+
+  execute({ base, limit = 10 }: CreateTableOptions ){
+    
+    let outputMessage = '';
+    for( let i = 1; i <= limit; i++ ) {
+      outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
     }
+
     return outputMessage;
   }
+  
 }
